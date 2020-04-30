d879778f48975f8f5e2c971497ea81711e564db1
Bademia Babu <bademia.shaik@itcinfotech.com>
2020-04-30 23:37:12 +0530

    zuul-service initial commit

---
+++ src/main/java/com/itc/demo/EmployeeZuulFilter.java
@@ -0,0 +1,45 @@
+package com.itc.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class EmployeeZuulFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		
+		System.out.println("In Zuul Filter");
+		
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest req = ctx.getRequest();
+		System.out.println(req.getMethod()+"   "+req.getRequestURL().toString());
+		System.out.println("prefilter"+String.format("%s request to %s",req.getMethod(),req.getRequestURL().toString()));
+		
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
